52306af82a510d7b3aff9a7819527562434d4118
NKINC <NKINC@users.noreply.github.com>
2020-05-08 18:22:16 -0400

    Add files via upload

--- Download/Form1.vb
+++ Download/Form1.vb
@@ -111,10 +111,10 @@ Public Class Form1
         Public UploadDate As DateTimeOffset
         Public Width As Integer
         Public Height As Integer
-        Public VideoQuality As YoutubeExplode.Models.MediaStreams.VideoQuality
+        Public VideoQuality As YoutubeExplode.Videos.Streams.VideoQuality 'YoutubeExplode.Models.MediaStreams.VideoQuality
         Public VideoQualityLabel As String
-        Public VideoEncoding As YoutubeExplode.Models.MediaStreams.VideoEncoding
-        Public MuxStreamInfoObject As YoutubeExplode.Models.MediaStreams.MuxedStreamInfo
+        Public VideoEncoding As YoutubeExplode.Videos.Streams.VideoResolution 'YoutubeExplode.Models.MediaStreams.VideoEncoding
+        Public MuxStreamInfoObject As YoutubeExplode.Videos.Streams.MuxedStreamInfo 'YoutubeExplode.Videos.Streams.VideoStreamInfoExtensions 'YoutubeExplode.Models.MediaStreams.MuxedStreamInfo
     End Structure
     Public Function getStrucVideoMax(ByRef strucVideoObjects As System.Collections.Generic.List(Of strucVideo)) As strucVideo
         If Not strucVideoObjects Is Nothing Then
@@ -128,7 +128,7 @@ Public Class Form1
                             End If
                         Else
                             If strucVideoObj.Format.ToString().ToLower().Contains(cmbFormat.Items(cmbFormat.SelectedIndex).ToString().ToLower().TrimStart(".")) Then
-                                If strucVideoObj.MuxStreamInfoObject.Resolution.Height >= strucVideoMax.MuxStreamInfoObject.Resolution.Height Then
+                                If strucVideoObj.Height >= strucVideoMax.Height Then
                                     strucVideoMax = strucVideoObj
                                 End If
                             End If
@@ -155,7 +155,7 @@ Public Class Form1
                             End If
                         Else
                             If strucVideoObj.Format.ToString().ToLower().Contains(cmbFormat.Items(cmbFormat.SelectedIndex).ToString().ToLower().TrimStart(".")) Then
-                                If strucVideoObj.MuxStreamInfoObject.Resolution.Height >= recommendedHeight Then
+                                If strucVideoObj.Height >= recommendedHeight Then
                                     strucVideoMax = strucVideoObj
                                 End If
                             End If
@@ -175,49 +175,66 @@ Public Class Form1
         Try
             enableButtons()
             Dim link As String = New Uri(txtYoutubeURL.Text.Trim()).ToString()
-            Dim id As String = YoutubeExplode.YoutubeClient.ParseVideoId(txtYoutubeURL.Text.Trim())
-            Dim client As New YoutubeExplode.YoutubeClient()
-            Dim videoTask As Task(Of YoutubeExplode.Models.Video) = client.GetVideoAsync(id)
-            Dim video As YoutubeExplode.Models.Video = Await videoTask
+            'Dim id As String = YoutubeExplode.YoutubeClient.ParseVideoId(txtYoutubeURL.Text.Trim())
+            'Dim client As New YoutubeExplode.YoutubeClient()
+            Dim youtube As New YoutubeExplode.YoutubeClient()
+            Dim video = Await youtube.Videos.GetAsync((txtYoutubeURL.Text.Trim()))
+            Dim id As String = video.Id
+            Dim streamManifest = Await youtube.Videos.Streams.GetManifestAsync(id)
+            'Dim videoTask As Task(Of YoutubeExplode.Videos.Streams.) = client.GetVideoAsync(id)
+            'Dim video As YoutubeExplode.Models.Video = Await videoTask
+            'Dim video As YoutubeExplode.Videos.Streams.MuxedStreamInfo = client ' = Await videoTask
+            '// Get highest quality muxed stream
+            Dim streamInfo As List(Of YoutubeExplode.Videos.Streams.MuxedStreamInfo) = streamManifest.GetMuxed.ToList
 
             cmbFormat.Items.Clear()
             cmbResolution.Items.Clear()
             Dim resHighest As Integer = 0
 
-            Dim title As String = video.Title
-            Dim author As String = video.Author
-            Dim duration As TimeSpan = video.Duration
-            Dim streamInfoSetTask As Task(Of YoutubeExplode.Models.MediaStreams.MediaStreamInfoSet) = client.GetVideoMediaStreamInfosAsync(video.Id)
-            Dim streamInfoSet As YoutubeExplode.Models.MediaStreams.MediaStreamInfoSet = Await streamInfoSetTask
-            Dim mediaStreamInfo As YoutubeExplode.Models.MediaStreams.MediaStreamInfo = YoutubeExplode.Models.MediaStreams.Extensions.WithHighestVideoQuality(streamInfoSet.Muxed)
-            Dim streamInfo As YoutubeExplode.Models.MediaStreams.MediaStreamInfo = Nothing
-            Dim strUR1 As String = mediaStreamInfo.Url
+            'Dim title As String = video.Title
+            'Dim streamInfo = streamManifest
+            'Dim author As String = video.AdaptiveType
+            'Dim duration As TimeSpan = video.Duratio
+            'Dim streamInfoSetTask As Task(Of YoutubeExplode.Videos.Streams..Models.MediaStreams.MediaStreamInfoSet) = client.GetVideoMediaStreamInfosAsync(video.Id)
+            'Dim streamInfoSet As YoutubeExplode.Models.MediaStreams.MediaStreamInfoSet = Await streamInfoSetTask
+            'Dim mediaStreamInfo As YoutubeExplode.Models.MediaStreams.MediaStreamInfo = YoutubeExplode.Models.MediaStreams.Extensions.WithHighestVideoQuality(streamInfoSet.Muxed)
+            'Dim streamInfo As YoutubeExplode.Models.MediaStreams.MediaStreamInfo = Nothing
+            'Dim strUR1 As String = .Url
             Dim mediaFormats As New List(Of String)
             mediaFormats.Add(".*")
             videoInfosTemp1 = New System.Collections.Generic.List(Of strucVideo)
-            For Each streamResolution As YoutubeExplode.Models.MediaStreams.MuxedStreamInfo In streamInfoSet.Muxed.ToArray
-                Dim mediaFormat As String = YoutubeExplode.Models.MediaStreams.Extensions.GetFileExtension(streamResolution.Container).ToString.ToLower()
-                If mediaFormats.Contains(mediaFormat) Or mediaFormats.Contains(".*") Then
+            'Dim youtube As New YoutubeExplode.YoutubeClient()
+            '// You can specify video ID Or URL
+
+            'Dim title As String = video.Title ' // "Infected Mushroom - Spitfire [Monstercat Release]"
+            'Dim author As String = video.Author // "Monstercat"
+            'Dim duration As TimeSpan = video.Duration '; // 00:  07:14
+            'video
+
+            For Each streamResolution As YoutubeExplode.Videos.Streams.MuxedStreamInfo In streamInfo.ToArray()
+                'Dim mediaFormat As String = YoutubeExplode.Models.MediaStreams.Extensions.GetFileExtension(streamResolution.Container).ToString.ToLower()
+                Dim mediaFormat As YoutubeExplode.Videos.Streams.VideoResolution = streamResolution.Resolution
+                If streamResolution.Container = YoutubeExplode.Videos.Streams.Container.Mp4 Then 'mediaFormats.Contains(mediaFormat) Or mediaFormats.Contains(".*") Then
                     Dim videStruc As New strucVideo()
-                    videStruc.Title = title
-                    videStruc.Format = mediaFormat
+                    videStruc.Title = video.Title
+                    videStruc.Format = streamResolution.Container.ToString() 'mediaFormat.Height.ToString
                     videStruc.MuxStreamInfoObject = streamResolution
                     videStruc.Description = video.Description
                     videStruc.Author = video.Author
                     videStruc.Duration = video.Duration
-                    videStruc.Id = video.Id
+                    videStruc.Id = video.Id.ToString
                     videStruc.Width = streamResolution.Resolution.Width
                     videStruc.VideoQualityLabel = streamResolution.VideoQualityLabel
                     videStruc.Height = streamResolution.Resolution.Height
-                    videStruc.Keywords = video.Keywords.ToList
+                    videStruc.Keywords = video.Keywords.ToList()
                     videStruc.UploadDate = video.UploadDate
                     videStruc.VideoQuality = streamResolution.VideoQuality
-                    videStruc.VideoEncoding = streamResolution.VideoEncoding
+                    videStruc.VideoEncoding = streamResolution.Resolution
                     videoInfosTemp1.Add(videStruc)
-                    Dim vidInfo As New YoutubeExtractor.VideoInfo(YoutubeExtractor.VideoType.Mp4, title, streamResolution.Url, streamResolution.Resolution.Height)
-                    Dim streamMaxResolution As YoutubeExplode.Models.MediaStreams.MuxedStreamInfo = Nothing
-                    If Not cmbFormat.Items.Contains(mediaFormat) Then
-                        cmbFormat.Items.Add(mediaFormat)
+                    Dim vidInfo As New YoutubeExtractor.VideoInfo(YoutubeExtractor.VideoType.Mp4, video.Title, streamResolution.Url, streamResolution.Resolution.Height)
+                    Dim streamMaxResolution As YoutubeExplode.Videos.Streams.MuxedStreamInfo = Nothing 'YoutubeExplode.Videos.Streams.VideoStreamInfoExtensions = Nothing
+                    If Not cmbFormat.Items.Contains(videStruc.Format.ToString()) Then
+                        cmbFormat.Items.Add(videStruc.Format.ToString())
                     End If
                     If cmbFormat.Items.Count > 0 Then
                         If cmbFormat.Items.Contains("mp4") Then
@@ -226,12 +243,12 @@ Public Class Form1
                             cmbFormat.SelectedIndex = 0
                         End If
                     End If
-                    If streamInfo Is Nothing Then
-                        streamInfo = streamResolution
-                        streamMaxResolution = streamInfo
-                    ElseIf streamResolution.Resolution.Height >= streamResolution.Resolution.Height Then
-                        streamInfo = streamResolution
-                        streamMaxResolution = streamInfo
+                    If streamMaxResolution Is Nothing Then
+                        'streamInfo = streamResolution
+                        streamMaxResolution = streamResolution 'streamInfo
+                    ElseIf streamResolution.Resolution.Height >= streamMaxResolution.Resolution.Height Then
+                        'streamInfo = streamResolution
+                        streamMaxResolution = streamResolution 'streamInfo
                     End If
                     If Not cmbResolution.Items.Contains(streamResolution.VideoQualityLabel.ToString()) Then
                         cmbResolution.Items.Add(streamResolution.VideoQualityLabel.ToString())
@@ -313,13 +330,14 @@ Public Class Form1
         r = New Regex("[^a-zA-Z0-9]")
         Return r.Replace(path, "-"c).ToString().Replace(" ", "-").Replace(" ", "-").Replace("--", "-").Replace("--", "-").Replace("--", "-").Replace("-.", "."c).TrimEnd("-"c)
     End Function
+    Public fileSizeMegaBytes As Long = 0
     Private Sub YoutubeDownloadProgressChangedEventHandler(ByVal sender As Object, ByVal e As ProgressEventArgs)
         If cancelYouTubeAsyncDownload Then
             Button1.Text = "Download Youtube File"
             e.Cancel = True
         End If
         ProgressBar1.Value = e.ProgressPercentage
-        Label1.Text = "Downloading: " & IIf(ProgressBar1.Value < 10, "0", "") & ProgressBar1.Value & "% - " & CInt(CSng(videoBytesLength * (e.ProgressPercentage / 100)) / 1024 / 1024) & " MB" & " - " & txtFileName.Text
+        Label1.Text = "Downloading: " & IIf(ProgressBar1.Value < 10, "0", "") & ProgressBar1.Value & "% - " & CInt(fileSizeMegaBytes - CInt(CSng(videoBytesLength * (e.ProgressPercentage / 100)) / 1024 / 1024)) & " MB remaining : " & CInt(CSng(videoBytesLength * (e.ProgressPercentage / 100)) / 1024 / 1024) & " MB of " & fileSizeMegaBytes & " MB - " & txtFileName.Text
         Me.Update()
         Application.DoEvents()
         If ProgressBar1.Value = 100 Then
@@ -328,7 +346,7 @@ Public Class Form1
     End Sub
     Private Sub DownloadProgressChangedEventHandler(ByVal sender As Object, ByVal e As System.Net.DownloadProgressChangedEventArgs)
         ProgressBar1.Value = e.ProgressPercentage
-        Label1.Text = "Downloading: " & IIf(ProgressBar1.Value < 10, "0", "") & ProgressBar1.Value & "% - " & CInt(CSng(videoBytesLength * (e.ProgressPercentage / 100)) / 1024 / 1024) & " MB" & " - " & txtFileName.Text
+        Label1.Text = "Downloading: " & IIf(ProgressBar1.Value < 10, "0", "") & ProgressBar1.Value & "% - " & CInt(fileSizeMegaBytes - CInt(CSng(videoBytesLength * (e.ProgressPercentage / 100)) / 1024 / 1024)) & " MB remaining : " & CInt(CSng(videoBytesLength * (e.ProgressPercentage / 100)) / 1024 / 1024) & " MB of " & fileSizeMegaBytes & " MB - " & txtFileName.Text
     End Sub
 
     Private Sub AsyncCompletedEventHandler(ByVal sender As Object, ByVal e As System.ComponentModel.AsyncCompletedEventArgs)
@@ -358,18 +376,20 @@ Public Class Form1
     Private Sub btnLoad_Click(sender As Object, e As EventArgs) Handles btnLoad.Click
         Try
             videoBytesLength = 0
+            fileSizeMegaBytes = 0
             enableButtons()
             Dim videoMax As strucVideo = getStrucVideoMax(videoInfosTemp1)
             Dim initialDirectory As String = GetSetting(Application.ProductName, "Settings", "InitialDirectory", CStr(Application.StartupPath)).ToString().TrimEnd("\"c) & "\"
             txtFileName.Text = initialDirectory & RemoveIllegalPathCharacters(videoMax.Title) & "."c & cmbFormat.Items(cmbFormat.SelectedIndex).ToString.TrimStart("."c) & ""
-            txtYoutubeDownloadURL.Text = videoMax.MuxStreamInfoObject.Url.ToString()
+            txtYoutubeDownloadURL.Text = videoMax.MuxStreamInfoObject.ToString
             txtYoutubeDownloadURL.ForeColor = Color.DarkGreen
             btnDownloadVideo.Visible = True
             Button1.Visible = True
             Button3.Visible = True
             Label1.Text = "Status: Success - Click a download button to procceed."
-            Label1.Text &= Environment.NewLine & "File Size: " & CInt(videoMax.MuxStreamInfoObject.Size / 1024 / 1024) & " MB"
-            videoBytesLength = CInt(videoMax.MuxStreamInfoObject.Size)
+            Label1.Text &= Environment.NewLine & "File Size: " & CInt(CInt(videoMax.MuxStreamInfoObject.Size.TotalBytes) / CInt(1024) / CInt(1024)) & " MB"
+            fileSizeMegaBytes = CInt(videoMax.MuxStreamInfoObject.Size.TotalBytes / 1024 / 1024)
+            videoBytesLength = CInt(videoMax.MuxStreamInfoObject.Size.TotalBytes)
 
         Catch ex As Exception
             Label1.Text = "Status: Error - " & ex.Message.ToString() & Environment.NewLine & "info: " & ex.StackTrace.ToString()
@@ -480,8 +500,9 @@ Public Class Form1
                             Button1.Visible = True
                             Button3.Visible = True
                             Label1.Text = "Status: Success - Click a download button to procceed."
-                            Label1.Text &= Environment.NewLine & "File Size: " & CInt(videoMax.MuxStreamInfoObject.Size / 1024 / 1024) & " MB"
-                            videoBytesLength = CInt(videoMax.MuxStreamInfoObject.Size)
+                            Label1.Text &= Environment.NewLine & "File Size: " & CInt(videoMax.MuxStreamInfoObject.Size.TotalBytes / 1024 / 1024) & " MB"
+                            fileSizeMegaBytes = CInt(videoMax.MuxStreamInfoObject.Size.TotalBytes / 1024 / 1024)
+                            videoBytesLength = CInt(videoMax.MuxStreamInfoObject.Size.TotalBytes)
                         End If
                     Catch ex2 As Exception
                         Err.Clear()
@@ -516,8 +537,9 @@ Public Class Form1
             Button1.Visible = True
             Button3.Visible = True
             Label1.Text = "Status: Success - Click a download button to procceed."
-            Label1.Text &= Environment.NewLine & "File Size: " & CInt(videoMax.MuxStreamInfoObject.Size / 1024 / 1024) & " MB"
-            videoBytesLength = CInt(videoMax.MuxStreamInfoObject.Size)
+            Label1.Text &= Environment.NewLine & "File Size: " & CInt(videoMax.MuxStreamInfoObject.Size.TotalBytes / 1024 / 1024) & " MB"
+            fileSizeMegaBytes = CInt(videoMax.MuxStreamInfoObject.Size.TotalBytes / 1024 / 1024)
+            videoBytesLength = CInt(videoMax.MuxStreamInfoObject.Size.TotalBytes)
         Catch ex As Exception
             Err.Clear()
         End Try
